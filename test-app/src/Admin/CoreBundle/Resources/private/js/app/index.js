d525d73ebb5d137716cc4aa575e41830288af05d
Hugo Alliaume <kocal@live.fr>
2018-08-31 10:44:26 +0200

    chore(test-app): setup usage of a JSON file

--- test-app/src/Admin/CoreBundle/Resources/private/js/app/index.js
+++ test-app/src/Admin/CoreBundle/Resources/private/js/app/index.js
@@ -13,6 +13,7 @@ import directives from './directives';
 import filters from './filters';
 import mixins from './mixins';
 import utils from './utils';
+import { foo } from './my-json-file.json';
 
 global.App = {
   ...components,
@@ -21,6 +22,8 @@ global.App = {
   ...filters,
   ...utils,
 };
+
+console.log(foo);
 
 global.YproxComponentsBootstrap = function YproxComponentsBootstrap(Vue) {
   if (process.env.NODE_ENV === 'production') {
