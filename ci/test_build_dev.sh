0d5ddf1b9db2bc394cd612436634706daf91ec8d
Hugo Alliaume <kocal@live.fr>
2018-09-05 12:37:00 +0200

    refactor(testing): split tests into bash files

---
+++ ci/test_build_dev.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -ev
+
+test -f public/css/button.css
+test ! -f public/css/button.css.map
+test -f public/css/legacy.css
+test ! -f public/css/legacy.css.map
+test -f public/css/my-grid.css
+test ! -f public/css/my-grid.css.map
+
+test -f public/img/check.png
+test -f public/img/cross.png
+test -f public/img/mini_arrow_down.png
+test -f public/img/mini_arrow_up.png
+
+test -f public/js/storeLocatorUtils.min.js
+test ! -f public/js/storeLocatorUtils.min.js.map
+test -f public/js/core-app-admin.min.js
+test ! -f public/js/core-app-admin.min.js.map
+test -f public/js/core-app-front.min.js
+test ! -f public/js/core-app-front.min.js.map
+test -f public/js/vue.js
+test ! -f public/js/vue.min.js
+test ! -f public/js/vue.min.js.map
+test -f public/js/yprox-store-locator.min.js
+test ! -f public/js/yprox-store-locator.min.js.map
+test -f public/js/yprox-media-browser.min.js
+test ! -f public/js/yprox-media-browser.min.js.map
+
+test -f public/plugins/jQuery-Validation-Engine/css/validationEngine.jquery.css
+test -f public/plugins/jQuery-Validation-Engine/js/languages/jquery.validationEngine-fr.js
+test -f public/plugins/jQuery-Validation-Engine/js/jquery.validationEngine.js
