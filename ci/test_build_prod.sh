7f0ad5ec85ca882f051b8344507d22d75a197144
Hugo Alliaume <kocal@live.fr>
2018-12-25 21:35:45 +0100

    feat: Remove webpack handler (#140)
    BREAKING CHANGE: Yprox-CLI should stay light and simple, so I decided to remove the `webpack` handler in favour of the `rollup` handler.
    
    You have 4 solutions :
    
    - Use the [`rollup` handler](https://yprox-cli.netlify.com/handlers.html#rollup)
    - Use [Webpack Encore](https://github.com/symfony/webpack-encore) if you are building a Symfony application
    - Use [vue-cli](https://github.com/vuejs/vue-cli) if you are building a Single Page application
    - Use webpack and webpack-cli

--- ci/test_build_prod.sh
+++ ci/test_build_prod.sh
@@ -23,23 +23,14 @@ test -f public/img/mini_arrow_down.png
 test -f public/img/mini_arrow_up.png
 
 # Files built with Rollup
+test -f public/js/core-app-public.rollup.js
+test -f public/js/core-app-public.rollup.js.map
 test -f public/js/core-app-admin.rollup.js
 test -f public/js/core-app-admin.rollup.js.map
 test -f public/js/yprox-media-browser.rollup.js
 test -f public/js/yprox-media-browser.rollup.js.map
 test -f public/js/yprox-store-locator.rollup.js
 test -f public/js/yprox-store-locator.rollup.js.map
-
-# Files built with webpack
-test -f public/js/manifest.js
-test -f public/js/vendor.js
-test -f public/js/vendor.css
-test -f public/js/core-app-front.webpack.js
-test -f public/js/core-app-front.webpack.css
-test -f public/js/core-app-admin.webpack.js
-test -f public/js/yprox-media-browser.webpack.js
-test -f public/js/yprox-store-locator.webpack.js
-test -f public/js/lazy-imported-component.js
 
 test -f public/plugins/jQuery-Validation-Engine/css/validationEngine.jquery.css
 test -f public/plugins/jQuery-Validation-Engine/js/languages/jquery.validationEngine-fr.js
