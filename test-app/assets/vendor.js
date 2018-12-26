ce327854f6e9a3a6de2b19e88a09acc0bae51653
Hugo Alliaume <kocal@live.fr>
2018-12-26 16:19:28 +0100

    feat(api): add `API#isProduction()` (#145)
    Now it really check for `process.env.NODE_ENV`, not the mode.

--- test-app/assets/vendor.js
+++ test-app/assets/vendor.js
@@ -1,8 +1,8 @@
-module.exports = (cli, config) => ([
+module.exports = (api, config) => ([
   {
     handler: 'js',
-    src: cli.mode === 'production' ? 'node_modules/vue/dist/vue.min.js' : 'node_modules/vue/dist/vue.js',
-    concat: cli.mode === 'production' ? 'vue.min.js' : 'vue.js',
+    src: api.isProduction() ? 'node_modules/vue/dist/vue.min.js' : 'node_modules/vue/dist/vue.js',
+    concat: api.isProduction() ? 'vue.min.js' : 'vue.js',
     dest: config.path.js,
   },
 ]);
