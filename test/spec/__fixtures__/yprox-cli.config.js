ea286ee2c9faae9302ba23de2a4afdb6b282d83e
Hugo Alliaume <kocal@live.fr>
2019-01-07 09:05:07 +0100

    chore(spec): add scripts "build" and "lint"

--- test/spec/__fixtures__/yprox-cli.config.js
+++ test/spec/__fixtures__/yprox-cli.config.js
@@ -1,3 +1,5 @@
+// This file is ignored when running `yarn build` or `yarn lint`
+// The yProx-CLI config is located in the `package.json`, under `yproxCli` key.
 module.exports = {
   assets: {
     app: [{ handler: 'rollup', src: 'i-dont-exists/from-yprox-cli-config.js/index.js' }],
