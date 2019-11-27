fe901d12a8e9e696c483a011c8c50cd284e67d2c
Hugo Alliaume <kocal@live.fr>
2019-11-27 07:29:45 +0100

    build(deps): update @kocal/logger

--- lib/logger.ts
+++ lib/logger.ts
@@ -5,6 +5,15 @@ export { Logger } from '@kocal/logger';
 export const initLogger = (verbose = false): Logger => {
   return Logger.getLogger('yprox-cli', {
     level: verbose ? 'log' : 'info',
-    format: (ctx: Context, variables: Variables) => `[${ctx.chalk.blue(ctx.luxon.toFormat('HH:mm:ss'))}] ${ctx.levelColor(ctx.level)} :: ${ctx.message}`,
+    format: (ctx: Context, variables: Variables) => {
+      const date = new Date();
+      const dateFormatted = [
+        String(date.getHours()).padStart(2, '0'),
+        String(date.getMinutes()).padStart(2, '0'),
+        String(date.getSeconds()).padStart(2, '0'),
+      ].join(':');
+
+      return `[${ctx.chalk.blue(dateFormatted)}] ${ctx.levelColor(ctx.level)} :: ${ctx.message}`;
+    },
   });
 };
