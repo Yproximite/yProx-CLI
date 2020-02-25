0ab923a21b190fd416f66e0034077a9b3a1ff510
Hugo Alliaume <kocal@live.fr>
2020-02-25 07:08:44 +0100

    build(deps): use @rollup/plugin-node-resolve

--- lib/plugins/build/handlers/rollup.ts
+++ lib/plugins/build/handlers/rollup.ts
@@ -7,7 +7,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import builtins from 'rollup-plugin-node-builtins';
 import globals from 'rollup-plugin-node-globals';
-import nodeResolve from 'rollup-plugin-node-resolve';
+import nodeResolve from '@rollup/plugin-node-resolve';
 import replace from 'rollup-plugin-replace';
 import { terser } from 'rollup-plugin-terser';
 import { EntryRollup } from '../../../../types/entry';
